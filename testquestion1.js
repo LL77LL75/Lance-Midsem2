18ccd0440ce7c806e5abd67777a767ba7edfac96
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-09-04 10:11:27 +0000

    Co-authored-by: Computhink-TrainerMaster <Computhink-TrainerMaster@users.noreply.github.com>

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,24 @@
+// variables
+let fallingObject;
+let floor;
+//setup
+function setup(){
+    new Canvas(600,400);
+    fallingObject = new Sprite()
+    fallingObject.x = 300;
+    fallingObject.y = 200;
+    fallingObject.width = 30;
+    fallingObject.height = 30;
+    fallingObject.collider = "dynamic";
+    floor = new Sprite(2400,20,1200,20)
+    floor.x = 0;
+    floor.y = 10;
+    floor.width = 2400;
+    floor.height = 20;
+    floor.collider = "static";
+    world.gravity.y = -10
+}
+function Draw(){
+    background(-1)
+}
 
