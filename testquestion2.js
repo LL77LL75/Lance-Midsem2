5b64373b0a80d6d7f29ef5e35f5bee814d0e8aed
LL77LL75 <180167098+LL77LL75@users.noreply.github.com>
2026-09-04 10:25:00 +0000

    and push

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,30 @@
+// variables
+let movingObject;
+let floor;
+//setup
+function setup(){
+    new Canvas(600,400);
+    movingObject = new Sprite()
+    movingObject.x = 300;
+    movingObject.y = 200;
+    movingObject.width = 30;
+    movingObject.height = 30;
+    movingObject.collider = "dynamic";
+    movingObject.mass = 1;
+    movingObject.bouciness = -1000;
+    floor = new Sprite(2400,20,1200,20)
+    floor.x = 0;
+    floor.y = 390;
+    floor.width = 2400;
+    floor.height = 20;
+    floor.collider = "static";
+    world.gravity.y = 10
+}
+function draw(){
+    background(220);
+    camera.x = movingObject.x;
+    if (kb.presses(" ")){
+        movingObject.vel.y = -5;
+    }
+}
 
